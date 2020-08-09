03f77ce32e22b77e9b5236186341d208f6931e0d
e75n3r4 <vijay.bujula@mckesson.com>
2020-08-09 00:12:28 -0400

    removed dupplicate main class

--- src/main/java/com/mck/ps/example/demo/DemoApplication.java
+++ src/main/java/com/mck/ps/example/demo/DemoApplication.java
@@ -3,11 +3,11 @@ package com.mck.ps.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(DemoApplication.class, args);
+	// }
 
 }
